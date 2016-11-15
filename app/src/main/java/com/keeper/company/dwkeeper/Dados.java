f58abc0d55b2ebfc496ce5c2af64ac765b49321c
vipkry <lolpsxvipkry2@gmail.com>
2016-11-15 18:20:43 -0200

    activity dados completa

--- app/src/main/java/com/keeper/company/dwkeeper/Dados.java
+++ app/src/main/java/com/keeper/company/dwkeeper/Dados.java
@@ -4,16 +4,20 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
 
 public class Dados extends AppCompatActivity {
 
+    TextView texto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dados);
+        texto = (TextView) findViewById(R.id.textView);
     }
 
     // método para simular o rolar de um dado qualquer
@@ -21,7 +25,7 @@ public class Dados extends AppCompatActivity {
         int lados = Integer.parseInt(v.getTag().toString());
         Random rand = new Random();
         int res =  rand.nextInt(lados) + 1; // acrescenta 1 pq começa no zero
-        Toast.makeText(Dados.this, "" + res , Toast.LENGTH_SHORT).show();
+        texto.setText("Ultimo d" + lados + ": " + res);
     }
 
 }
