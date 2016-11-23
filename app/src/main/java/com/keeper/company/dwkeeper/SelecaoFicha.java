af9bb5ca4a4794132f55ffeb1e8a1e9db4119e9e
Carolina Ransatto <carolinaransatto@id.uff.br>
2016-11-22 23:19:40 -0200

    Estruturas da ficha e da página de seleção de fichas feitas

---
+++ app/src/main/java/com/keeper/company/dwkeeper/SelecaoFicha.java
@@ -0,0 +1,13 @@
+package com.keeper.company.dwkeeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelecaoFicha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selecao_ficha);
+    }
+}
