9bcd4168e75e00b0567d21eb0920c2cb964c6ba5
Nicholas <nicholas.quintella@hotmail.com>
2016-12-04 15:35:25 -0200

    Mudancas do andre

---
+++ app/src/main/java/com/keeper/company/dwkeeper/ImageHelper.java
@@ -0,0 +1,25 @@
+package com.keeper.company.dwkeeper;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Nicholas on 23/11/2016.
+ */
+
+public class ImageHelper {
+    public boolean hasImage(Context context){
+        return true;
+    }
+
+    /*public Drawable getThumbnail(Context context){
+    }
+
+    public Drawable getImage(Context context){
+    }
+
+    private Drawable getScaledImage(Context context, int reqWidth, int reqHeight) {
+
+    }
+*/
+}
